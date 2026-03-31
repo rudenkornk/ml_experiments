54367876b95df649c9883b1616f79f21a1dae68f
Nikita Rudenko <rudenkornk@gmail.com>
2026-03-31 14:50:00 +0300

    refactor(project): adapt to ML projects

--- sitecustomize.py
+++
@@ -1,8 +0,0 @@
-"""Enable coverage for subprocesses."""
-
-import os
-
-if "COVERAGE_PROCESS_START" in os.environ:
-    import coverage
-
-    coverage.process_startup()
--- tests/test_cli/test_cli.py
+++
@@ -1,5 +0,0 @@
-from ml_experiments.utils import run_shell
-
-
-def test_cli() -> None:
-    run_shell(["python", "-m", "ml_experiments", "pass"])
